75d6405bbcc9a1a1ff8a21848a3c63b3541b752a
AF <gammamustang@gmail.com>
2020-05-10 23:25:50 +0200

    add TodoConfig class

---
+++ src/main/java/rest/TodoConfig.java
@@ -0,0 +1,9 @@
+package rest;
+
+import javax.ejb.ApplicationException;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api/v1")
+public class TodoConfig extends Application {
+}
